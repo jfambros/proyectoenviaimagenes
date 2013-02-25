274f72ba6f46a55441800a7992c65db1f73021ba
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-02-25 21:39:44 +0000

    inicio geolocalizacion

--- trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/camara/ObtieneFoto.java
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/camara/ObtieneFoto.java
@@ -5,22 +5,29 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.amber.proyecto.envia.imagenes.sw.EnviaImagenSW;
 import com.amber.proyecto.envia.imagenes.sw.R;
+import com.google.android.maps.GeoPoint;
 
 public class ObtieneFoto extends Activity{
 	private ManejoFoto preview;
-	Button buttonClick; // <2>
+	private FrameLayout frameLayout;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private GeoPoint currentGeoPoint;
 
 	  /** Called when the activity is first created. */
 	  @Override
@@ -29,17 +36,68 @@ public class ObtieneFoto extends Activity{
 	    setContentView(R.layout.obtienefoto);
 
 	    preview = new ManejoFoto(this); // <3>
-	    ((FrameLayout) findViewById(R.id.preview)).addView(preview); // <4>
+	    frameLayout = ((FrameLayout) findViewById(R.id.preview));
+	    frameLayout.addView(preview);// <4>
+	    frameLayout.setOnClickListener(framePres);
 
-	    buttonClick = (Button) findViewById(R.id.btnObtieneFoto);
-	    buttonClick.setOnClickListener(new OnClickListener() {
-	      public void onClick(View v) { // <5>
-	        preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-	      }
-	    });
-
+	    
+	    locationListener = new LocationListener() {
+			
+			@Override
+			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderEnabled(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProviderDisabled(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onLocationChanged(Location arg0) {
+				localizacion();
+				
+			}
+		};
+		
+		locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+	    localizacion();
+	    
 	  }
+	  
+	  public void localizacion(){
+	    	setCurrentGeoPoint(new GeoPoint( 
+	        		(int)(locationManager.getLastKnownLocation(
+	        				LocationManager.GPS_PROVIDER).getLatitude()*1000000.0),
+	        		(int)(locationManager.getLastKnownLocation(
+	        				LocationManager.GPS_PROVIDER).getLongitude()*1000000.0)));
+	    }
+	  
+		public void setCurrentGeoPoint(GeoPoint currentGeoPoint) {
+			this.currentGeoPoint = currentGeoPoint;
+		}
 
+		public GeoPoint getCurrentGeoPoint() {
+			return currentGeoPoint;
+		}
+
+	  
+	  
+	  private OnClickListener framePres = new OnClickListener() {
+			
+		public void onClick(View v) {
+			preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+		}
+	};
 	  // Called when shutter is opened
 	  ShutterCallback shutterCallback = new ShutterCallback() { // <6>
 	    public void onShutter() {
