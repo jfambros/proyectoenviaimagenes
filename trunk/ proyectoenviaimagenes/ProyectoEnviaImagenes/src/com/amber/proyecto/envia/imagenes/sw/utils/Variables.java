fdc1e19b2f965b08efbef1de11789e20e957a58e
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-03-08 21:12:45 +0000

    coordenadas, no se obtiene la dirección, ya envía al servidor

--- trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
@@ -2,6 +2,7 @@ package com.amber.proyecto.envia.imagenes.sw.utils;
 
 public class Variables {
 	
-	public static final String HOST = "10.0.2.2";
+	//public static final String HOST = "10.0.2.2";
+	public static final String HOST = "192.168.1.201";
 
 }
