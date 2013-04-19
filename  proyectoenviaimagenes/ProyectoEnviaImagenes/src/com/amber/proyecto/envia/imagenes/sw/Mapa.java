7b421f933d419544c3b7edfcfbccf086190be472
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-04-19 01:34:54 +0000

    MApa v1

---  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/Mapa.java
+++  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/Mapa.java
@@ -6,37 +6,35 @@ import android.view.Menu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Mapa extends FragmentActivity {
-	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-	  static final LatLng KIEL = new LatLng(53.551, 9.993);
+public class Mapa extends android.support.v4.app.FragmentActivity {
+	  static final LatLng OAX = new LatLng(17.071554, -96.71917);
 	  private GoogleMap map;
 
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.mapa);
-	    map = ((MapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa))
+
+	   
+	    map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa))
 	        .getMap();
-	    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-	        .title("Hamburg"));
-	    Marker kiel = map.addMarker(new MarkerOptions()
-	        .position(KIEL)
-	        .title("Kiel")
-	        .snippet("Kiel is cool")
+	    Marker hamburg = map.addMarker(new MarkerOptions().position(OAX)
+	        .title("Oaxaca")
 	        .icon(BitmapDescriptorFactory
 	            .fromResource(R.drawable.ic_launcher)));
 
 	    // Move the camera instantly to hamburg with a zoom of 15.
-	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(OAX, 15));
 
 	    // Zoom in, animating the camera.
 	    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	    
 	  }
 
 	  @Override
