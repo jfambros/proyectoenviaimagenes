e846cf5604feff75ec4b07c2697dc5244ef7bc75
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-04-19 20:12:07 +0000

    Mapa v2

---
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/ImagenParcelable.java
@@ -0,0 +1,110 @@
+package com.amber.proyecto.envia.imagenes.sw.utils;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ImagenParcelable implements Parcelable{
+	private String nombreImagen;
+	private double latitud;
+	private double longitud;
+	private int idCategoria;
+	private String comentario;
+	private String nombreCategoria;
+
+	public String getNombreCategoria() {
+		return nombreCategoria;
+	}
+
+	public void setNombreCategoria(String nombreCategoria) {
+		this.nombreCategoria = nombreCategoria;
+	}
+
+	public String getNombreImagen() {
+		return nombreImagen;
+	}
+
+	public void setNombreImagen(String nombreImagen) {
+		this.nombreImagen = nombreImagen;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public static final Parcelable.Creator<ImagenParcelable> CREATOR  = new Creator<ImagenParcelable>() {
+		
+		@Override
+		public ImagenParcelable[] newArray(int size) {
+			return new ImagenParcelable[size];
+		}
+		
+		@Override
+		public ImagenParcelable createFromParcel(Parcel in) {
+			return new ImagenParcelable(in);
+		}
+	};
+	
+	
+	public ImagenParcelable(){
+		
+	}
+	
+	public ImagenParcelable(Parcel in){
+		readFromParcel(in);
+		
+	}
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(nombreImagen);
+		dest.writeDouble(latitud);
+		dest.writeDouble(longitud);
+		dest.writeInt(idCategoria);
+		dest.writeString(comentario);
+		dest.writeString(nombreCategoria);
+		
+	}
+	
+	private void readFromParcel(Parcel in){
+		nombreImagen = in.readString();
+		latitud = in.readDouble();
+		longitud = in.readDouble();
+		idCategoria = in.readInt();
+		comentario = in.readString();
+		nombreCategoria = in.readString();
+	}
+
+}
