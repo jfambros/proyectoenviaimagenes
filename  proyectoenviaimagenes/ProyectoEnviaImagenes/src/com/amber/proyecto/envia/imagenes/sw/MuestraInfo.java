0169daabb2a9fe4f85d5429709fe9179247829c0
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-04-20 22:57:45 +0000

    muestraInfo v1

---  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/MuestraInfo.java
+++  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/MuestraInfo.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MuestraInfo extends Activity{
+	private Bundle bundle;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
