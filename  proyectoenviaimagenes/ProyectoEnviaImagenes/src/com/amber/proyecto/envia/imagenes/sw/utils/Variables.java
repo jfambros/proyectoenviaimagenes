3743822da27fa32377d782e5b09e71355c989577
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-04-20 02:15:12 +0000

    Mapa v3

---  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
+++  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
@@ -7,7 +7,7 @@ import android.os.Environment;
 public class Variables {
 	
 	//public static final String HOST = "10.0.2.2";
-	public static final String HOST = "192.168.1.201";
+	public static final String HOST = "192.168.1.64";
 	public static final String tipoArchivo = ".jpg";
 	public static final String ruta = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) +File.separator;
 
