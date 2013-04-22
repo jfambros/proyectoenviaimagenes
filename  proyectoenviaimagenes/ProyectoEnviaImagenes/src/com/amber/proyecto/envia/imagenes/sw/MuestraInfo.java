2ba21a9d0a960e0854486a5d8f92fd58dca2d3ee
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-04-22 20:05:40 +0000

    Mapa v4

---  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/MuestraInfo.java
+++  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/MuestraInfo.java
@@ -3,16 +3,26 @@ package com.amber.proyecto.envia.imagenes.sw;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.amber.proyecto.envia.imagenes.sw.utils.ImagenParcelable;
+import com.amber.proyecto.envia.imagenes.sw.utils.MuestraImagen;
+import com.amber.proyecto.envia.imagenes.sw.utils.Variables;
 
 public class MuestraInfo extends Activity{
 	private Bundle bundle;
+	private ImagenParcelable datosImagen = new ImagenParcelable();
+	private String ruta = "http://"+Variables.HOST+"/pags/";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.muestrainfo);
-		
 		bundle = getIntent().getExtras();
-		Log.i("nombreImagen ",bundle.getString("nombreImagen"));
+		datosImagen = bundle.getParcelable("datosImagen");
+		new MuestraImagen((ImageView)findViewById(R.id.ivImagenMuestraInfo)).execute(ruta+datosImagen.getNombreImagen());
+		
+			        
+		Log.i("nombreImagen ",datosImagen.getNombreImagen()+" "+datosImagen.getNombreCategoria());
 	}
 }
---
+++  proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/MuestraImagen.java
@@ -0,0 +1,40 @@
+package com.amber.proyecto.envia.imagenes.sw.utils;
+
+import java.io.InputStream;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class MuestraImagen extends AsyncTask<String, Void, Bitmap> {
+	  private ImageView bmImage;
+
+	  public MuestraImagen(ImageView bmImage) {
+	      this.bmImage = bmImage;
+	  }
+
+	  protected Bitmap doInBackground(String... urls) {
+	      String urldisplay = urls[0];
+	      Bitmap mIcon11 = null;
+	      try {
+			  BitmapFactory.Options options = new BitmapFactory.Options();
+			     options.inSampleSize = 2;
+	        InputStream in = new java.net.URL(urldisplay).openStream();
+
+	        mIcon11 = BitmapFactory.decodeStream(in, null, options);
+	        in.close();
+	      } catch (Exception e) {
+	          Log.e("Error", e.getMessage());
+	          e.printStackTrace();
+	      }
+	      return mIcon11;
+	  }
+
+	  protected void onPostExecute(Bitmap result) {
+
+		   
+	      bmImage.setImageBitmap(result);
+	  }
+	}
