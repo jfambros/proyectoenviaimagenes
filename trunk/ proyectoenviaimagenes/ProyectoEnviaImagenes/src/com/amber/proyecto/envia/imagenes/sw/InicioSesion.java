a8ef7af8dae6158df4b21d4a0e4b923d9b72c3d0
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-05-06 20:45:11 +0000

    Contactos

--- trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/InicioSesion.java
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/InicioSesion.java
@@ -9,22 +9,29 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
 
-import com.amber.proyecto.envia.imagenes.sw.utils.Cifrado;
-import com.amber.proyecto.envia.imagenes.sw.utils.Variables;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.amber.proyecto.envia.imagenes.sw.usuario.PrincipalUsuario;
+import com.amber.proyecto.envia.imagenes.sw.utils.Cifrado;
+import com.amber.proyecto.envia.imagenes.sw.utils.Variables;
 
 public class InicioSesion extends Activity{
 	private EditText etCorreoE;
 	private EditText etContra;
 	private Button btnInicioSesion;
+	private String correoUsuario;
+	private String nombreUsuario;
+	private ImageView ivAtrasInicioSesion;
+	private Bundle bundle;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,17 +40,60 @@ public class InicioSesion extends Activity{
 		etContra = (EditText)findViewById(R.id.etContraInicioSesion);
 		btnInicioSesion = (Button)findViewById(R.id.btnIniciarSesion);
 		btnInicioSesion.setOnClickListener(btnInicioSesionPres);
+		
+		ivAtrasInicioSesion = (ImageView)findViewById(R.id.ivAtrasInicioSesion);
+		ivAtrasInicioSesion.setOnClickListener(ivAtrasInicioSPres);
+		
+		bundle = getIntent().getExtras();
 	}
+	
+	private OnClickListener ivAtrasInicioSPres = new OnClickListener() {
+		public void onClick(View v) {
+			Intent intent = new Intent();
+    		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			intent.setClass(InicioSesion.this, Principal.class);
+			startActivity(intent);
+			finish();
+		}
+	};
+	
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 	
 	private OnClickListener btnInicioSesionPres = new OnClickListener() {
 		
 		public void onClick(View v) {
-			
+			if (etCorreoE.getText().length() != 0 && etContra.getText().length() !=0){
+				if (validaUsuario() == true){
+					Toast.makeText(InicioSesion.this, "Iniciando sesión...", Toast.LENGTH_LONG).show();
+					Intent intent = new Intent();
+		    		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		    		intent.putExtra("nombreUsuario", nombreUsuario);
+		    		intent.putExtra("correoUsuario", correoUsuario);
+		    		if (bundle.getInt("origen") == Variables.ENVIAIMAGEN){
+		    			intent.setClass(InicioSesion.this, PrincipalUsuario.class);
+		    		}
+		    		else{
+		    			intent.setClass(InicioSesion.this, PrincipalUsuario.class);
+		    		}
+		    		startActivity(intent);
+		    		finish();					
+				}
+				else{
+					Toast.makeText(InicioSesion.this, "Correo o contraseña incorrecto", Toast.LENGTH_LONG).show();
+				}
+			}else{
+				Toast.makeText(InicioSesion.this, "Captura los datos", Toast.LENGTH_LONG).show();
+			}
 		}
 	};
 	
 	
-	private void validaUsuario(){
+	private boolean validaUsuario(){
 		String URL = "http://"+Variables.HOST+"/pags/servicios.php";
 		SoapObject request;
 		String SOAP_ACTION="capeconnect:servicios:serviciosPortType#validaUsuario"; 
@@ -53,8 +103,8 @@ public class InicioSesion extends Activity{
 	
 		try{
 					request = new SoapObject(NAMESPACE, METHOD_NAME); 
-					request.addProperty("correoUsuario", etCorreoE.getText().toString());
-					request.addProperty("contraUsuario", Cifrado.getStringMessageDigest(etContra.getText().toString(),Cifrado.SHA256));
+					request.addProperty("correoElectronico", etCorreoE.getText().toString());
+					request.addProperty("contrasenia", Cifrado.getStringMessageDigest(etContra.getText().toString(),Cifrado.SHA256));
 				    
 					SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 					
@@ -67,23 +117,28 @@ public class InicioSesion extends Activity{
 
 					aht.call(SOAP_ACTION, envelope);
 					
-					SoapObject result =  (SoapObject) envelope.bodyIn;
-	                SoapPrimitive spResul = (SoapPrimitive) result.getProperty("result");
+					SoapObject result =  (SoapObject) envelope.getResponse();
+	                correoUsuario = ((SoapPrimitive)result.getProperty(0)).toString();
+	                nombreUsuario = ((SoapPrimitive)result.getProperty(1)).toString();
 	                
-					Log.i("resultado",spResul.toString());
-					Toast.makeText(InicioSesion.this, "Registro", Toast.LENGTH_LONG).show();
+	                if (correoUsuario.equals("null")){
+	                	return false;
+	                }else{
+	                	return true;
+	                }
+	                
+
 		    } 
 	    catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		} catch (XmlPullParserException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		}
 		
-		finally{
-			
-		}
 		
 	}
 }
---
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/usuario/SeleccionaContactos.java
@@ -0,0 +1,15 @@
+package com.amber.proyecto.envia.imagenes.sw.usuario;
+
+import com.amber.proyecto.envia.imagenes.sw.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SeleccionaContactos extends Activity{
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.seleccionacontactos);
+	}
+
+}
