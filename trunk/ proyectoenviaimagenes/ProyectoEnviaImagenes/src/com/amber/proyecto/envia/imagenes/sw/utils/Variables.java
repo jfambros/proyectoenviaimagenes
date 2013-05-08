3b4c382c5adc3bf3a9dcd67895d1d45557b33145
jfambros <jfambros@7f1d59c2-3f1e-85f2-1838-4376096572ff>
2013-05-08 17:58:46 +0000

    Listo correo, mejorar interfaz

--- trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
+++ trunk/ proyectoenviaimagenes/ProyectoEnviaImagenes/src/com/amber/proyecto/envia/imagenes/sw/utils/Variables.java
@@ -7,7 +7,7 @@ import android.os.Environment;
 public class Variables {
 	
 	//public static final String HOST = "10.0.2.2";
-	public static final String HOST = "192.168.1.201";
+	public static final String HOST = "192.168.1.210";
 	public static final String tipoArchivo = ".jpg";
 	public static final String ruta = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) +File.separator;
 	public static final int INICIO = 0;
